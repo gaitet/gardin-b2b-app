434130eb8af500d3d09b303f2c9ceec24d95a4f8
Тетяна Гаймус <gaitet.design@gmail.com>
2026-08-25 14:56:51 +0300

    Fix Vercel build

--- src/init.ts
+++ src/init.ts
@@ -5,7 +5,6 @@ import {
   viewport,
   init as initSDK,
   mockTelegramEnv,
-  type ThemeParams,
   retrieveLaunchParams,
   emitEvent,
   miniApp,
@@ -20,36 +19,46 @@ export async function init(options: {
   eruda: boolean;
   mockForMacOS: boolean;
 }): Promise<void> {
-  // Set @telegram-apps/sdk-react debug mode and initialize it.
   setDebug(options.debug);
   initSDK();
 
-  // Add Eruda if needed.
-  options.eruda && void import('eruda').then(({ default: eruda }) => {
-    eruda.init();
-    eruda.position({ x: window.innerWidth - 50, y: 0 });
-  });
+  options.eruda &&
+    void import('eruda').then(({ default: eruda }) => {
+      eruda.init();
+      eruda.position({
+        x: window.innerWidth - 50,
+        y: 0,
+      });
+    });
 
-  // Telegram for macOS has a ton of bugs, including cases, when the client doesn't
-  // even response to the "web_app_request_theme" method. It also generates an incorrect
-  // event for the "web_app_request_safe_area" method.
   if (options.mockForMacOS) {
     let firstThemeSent = false;
+
     mockTelegramEnv({
       onEvent(event, next) {
         if (event.name === 'web_app_request_theme') {
-          let tp: ThemeParams = {};
+          let tp = {};
+
           if (firstThemeSent) {
             tp = themeParams.state();
           } else {
             firstThemeSent = true;
-            tp ||= retrieveLaunchParams().tgWebAppThemeParams;
+            tp =
+              retrieveLaunchParams().tgWebAppThemeParams ?? {};
           }
-          return emitEvent('theme_changed', { theme_params: tp });
+
+          return emitEvent('theme_changed', {
+            theme_params: tp,
+          });
         }
 
         if (event.name === 'web_app_request_safe_area') {
-          return emitEvent('safe_area_changed', { left: 0, top: 0, right: 0, bottom: 0 });
+          return emitEvent('safe_area_changed', {
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0,
+          });
         }
 
         next();
@@ -57,7 +66,6 @@ export async function init(options: {
     });
   }
 
-  // Mount all components used in the project.
   backButton.mount.ifAvailable();
   initData.restore();
 
